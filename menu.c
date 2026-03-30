90be25e0f8f5c7fa0554bf2df409c03da511e966
trannguyennhuhoa <trannguyennhuhoa102@gmail.com>
2026-03-30 13:36:20 +0700

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,10 +2,10 @@
 #include "menu.h"
 
 void menu_hoocner() {
-    printf("--------SO DO HOOCNER--------\n");
+    printf("\n-------- SO DO HOOCNER --------\n");
     printf("1.Gia tri\n");
     printf("2.Tong quat\n");
-    printf("0. Thoat chuong trinh\n");
+    printf("0. Quay lai\n");
 }
 
 void clearInputBuffer(void) {
@@ -41,12 +41,20 @@ void xuLyMenuHoocner() {
                 hoocnertq();
                 break;
             case 0:
-                printf("Quay lai menu chinh.\n");
+                printf("Quay lai menu chinh.\n\n");
                 break;
             default:
                 printf("Lua chon khong hop le!\n");
         }
     } while (subChoice != 0);
+}
+
+void menu_maclaurin() {
+    printf("\n-------- KHAI TRIEN HAM --------\n");
+    printf("1. Ham sinx\n");
+    printf("2. Ham cos\n");
+    printf("3. Ham e^x\n");   
+    printf("0. Thoat chuong trinh\n");
 }
 
 void xulyMenuMaclaurin() {
@@ -79,22 +87,11 @@ void xulyMenuMaclaurin() {
     } while (subChoice != 0);
 }
 
-void menu_maclaurin() {
-    printf("--------KHAI TRIEN HAM--------\n");
-    printf("1.Ham sinx\n");
-    printf("2.Ham cos\n");
-    printf("3.Ham e mu x\n");   
-    printf("0. Thoat chuong trinh\n");
-    printf("Chon: ");
-}
-
 void menu_tachnghiem() {
-    printf("--------TACH NGHIEM--------\n");
-    printf("1.Dinh ly 3\n");
-    printf("2.Dinh ly 4\n");
-    printf("3.Dinh ly 5\n");   
+    printf("\n-------- TACH NGHIEM --------\n");
+    printf("1. Dinh ly 3\n");
+    printf("2. Dinh ly 4\n");
     printf("0. Thoat chuong trinh\n");
-    printf("Chon: ");
 }
 
 void xulyMenuTachNghiem() {
@@ -115,9 +112,6 @@ void xulyMenuTachNghiem() {
         case 2:
             dinhly4();
             break;
-        case 3:
-            //dinhly5();
-            break;
         case 0:
             printf("Quay lai menu chinh.\n");
             break;
@@ -128,13 +122,12 @@ void xulyMenuTachNghiem() {
 }
 
 void menu_timnghiem() {
-    printf("----------TIM NGHIEM-----------\n");
-    printf("1.Phuong phap chia doi\n");
-    printf("2.Phuong phap lap\n");
-    printf("3.Phuong phap tiep tuyen\n");
-    printf("4.Phuong phap day cung\n");
+    printf("\n---------- TIM NGHIEM -----------\n");
+    printf("1. Phuong phap chia doi\n");
+    printf("2. Phuong phap lap\n");
+    printf("3. Phuong phap tiep tuyen\n");
+    printf("4. Phuong phap day cung\n");
     printf("0. Thoat chuong trinh\n");
-    printf("Chon: ");
 }
 
 void xulyMenuTimNghiem() {
@@ -171,11 +164,11 @@ void xulyMenuTimNghiem() {
 }
 
 void menu() {
-    printf("----------PHUONG PHAP TINH-----------\n");
+    printf("\n---------- PHUONG PHAP TINH -----------\n");
     printf("1. So do Hoocner \n");
     printf("2. Khai trien ham theo Maclaurin\n");
     printf("3. Tach nghiem \n");
     printf("4. Tim nghiem\n");
     printf("0. Thoat chuong trinh\n");
-    printf("Chon: ");
+    printf("\nNhap lua chon: ");
 }
