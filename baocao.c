5c4fdabbb83d02c38d9fd87393971d3870b1f8fe
trannguyennhuhoa <trannguyennhuhoa102@gmail.com>
2026-03-30 13:48:23 +0700

    Update baocao.c

--- baocao.c
+++ baocao.c
@@ -46,7 +46,7 @@ void hoocnertq() {
     }
     
     for(int i=1;i<=n;i++) {
-        for(int j=0;j<n-i;j++) {
+        for(int j=0;j<n;j++) {
             b[i][j]=b[i-1][j]*c+b[i-1][j+1];
         }
     }
